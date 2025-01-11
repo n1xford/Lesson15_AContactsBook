744bd0741da6656ff2cae3d37d6bf3d112415116
n1xford <oleglogosha11@gmail.com>
2025-01-11 14:54:36 +0200

    add design part 2

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    photo = models.ImageField(upload_to='contacts_photo/',
+                              blank=True,
+                              null=True)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
+
+def contact_list(request):
+    contacts = Contact.object.all()
+    return render(request, 'app/contact_list.html', {'contacts': contacts})
